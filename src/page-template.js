0e0660dee8c45a95114679f9e4c1c79419a90391
alanam79 <halo2382001@gmail.com>
2022-06-05 12:44:25 -0400

    added bootstrap and iconic

--- src/page-template.js
+++ src/page-template.js
@@ -6,60 +6,61 @@ const generateTeam = (team) => {
 
   const generateManager = (manager) => {
     return `
-        <div class="m-5 row justify-content-around">
-        <div class="col">
+    <div class="container">
+    <div class="m-5 row justify-content-around">
+      <div class="col-12 col-md-6 col-xl-3 mb-3">
         <div class="card" style="width: 18rem;">
-        <div class="card-body">
-        <h5 class="card-title">Name: ${manager.getName()}</h5>
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${manager.getId()}</li>
-        <li class="list-group-item">Email: ${manager.getEmail()}</li>
-        <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
-        <li class="list-group-item">Role: ${manager.getRole()}</li>
-        </ul>
+          <div class="card-body">
+            <h5 class="card-header bg-secondary text-light">Name: ${manager.getName()}</h5>
+            <h5 class="card-header bg-secondary text-light">Role: ${manager.getRole()} <span class="oi oi-person"></span></h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">ID: ${manager.getId()}</li>
+              <li class="list-group-item">Email: ${manager.getEmail()}</li>
+              <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>        
+            </ul>
+          </div>
         </div>
-        </div>
-        </div>
-        </div>
+      </div>    
         `;
   }; 
 
   const generateEngineer = (engineer) => {
     return `
-        <div class="m-5 row justify-content-around">
-        <div class="col">
-        <div class="card" style="width: 18rem;">
+        
+    <div class="col-12 col-md-6 col-xl-3 mb-3">
+      <div class="card" style="width: 18rem;">
         <div class="card-body">
-        <h5 class="card-title">Name: ${engineer.getName()}</h5>
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${engineer.getId()}</li>
-        <li class="list-group-item">Email: ${engineer.getEmail()}</li>
-        <li class="list-group-item">Role: ${engineer.getRole()}</li>
-        <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
-        </ul>
+          <h5 class="card-header bg-secondary text-light">Name: ${engineer.getName()}</h5>
+          <h5 class="card-header bg-secondary text-light">Role: ${engineer.getRole()} <span class="oi oi-pencil"></span></h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: ${engineer.getEmail()}</li>        
+            <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
+          </ul>
         </div>
-        </div>
-        </div>
-        </div>
+      </div>
+    </div>
         `;
   };
 
   const generateIntern = (intern) => {
     return `
-        <div class="m-5 row justify-content-around">
-        <div class="col">
-        <div class="card" style="width: 18rem;">
+        
+    <div class="col-12 col-md-6 col-xl-3 mb-3">
+      <div class="card" style="width: 18rem;">
         <div class="card-body">
-        <h5 class="card-title">Name: ${intern.getName()}</h5>
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${intern.getId()}</li>
-        <li class="list-group-item">Email: ${intern.getEmail()}</li>
-        <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
+          <h5 class="card-header bg-secondary text-light">Name: ${intern.getName()}</h5>
+          <h5 class="card-header bg-secondary text-light">Role: ${intern.getRole()} <span class="oi oi-book"></span></h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: ${intern.getEmail()}</li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+          </ul>
         </div>
-        </div>
-        </div>
-        </div>
+      </div>
+    </div>
+  </div>
+  </div>
         `;
   };
 
@@ -97,6 +98,8 @@ module.exports = (team) => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" />
+      <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
       <link rel="stylesheet" href="style.css"></link>
@@ -105,7 +108,7 @@ module.exports = (team) => {
 
     <body>
     <header>
-    <h1 class"bg-danger">My Team</h1>
+    <h1 class="w-100 bg-success p-3 text-center text-light display-4">My Team</h1>
     </header>
     <main>
     
