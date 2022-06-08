617eccc6017663e885763958550c4fd33b7a7396
alanam79 <halo2382001@gmail.com>
2022-06-07 22:55:16 -0400

    completed testing

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -12,9 +12,46 @@ test("Can instatiate Engineer name from constructor", ()=>{
     expect(emp.name).toBe(name)
 })
 
+test("Can instatiate Engineer id from constructor", ()=>{
+    const id = "123"
+    const emp = new Engineer(id);
+    expect(emp.id).toBe(emp.id)
+})
+
+test("Can instatiate Engineer email from constructor", ()=>{
+    const email = "alanarmckeel@gmail.com"
+    const emp = new Engineer(email);
+    expect(emp.email).toBe(emp.email)
+})
+
+// test for all methods in class
 // test for all methods in class
 test("Can instatiate Engineer name from getName()", ()=>{
     const name = "Alana"
     const emp = new Engineer(name);
     expect(emp.getName()).toBe(name)
+})
+
+test("Can instatiate Engineer ID from getId()", ()=>{
+    const id = "123"
+    const emp = new Engineer(id);
+    expect(emp.getId()).toBe(emp.id)
+})
+
+test("Can instatiate Engineer email from getEmail()", ()=>{
+    const email = "alanarmckeel@mgmail.com"
+    const emp = new Engineer(email);
+    expect(emp.getEmail()).toBe(emp.email)
+})
+
+test("Can instatiate Engineer gitHub from gitHub()", ()=>{
+    const gitHub = "alanarm79"
+    const emp = new Engineer(gitHub);
+    expect(emp.github).toBe(emp.github)
+})
+
+test("Can instatiate Engineer role from getRole()", ()=>{
+    const role = "role"
+    const emp = new Engineer(role);
+    expect(emp.getRole()).toBe(emp.getRole())
 })
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -12,9 +12,46 @@ test("Can instatiate Intern name from constructor", ()=>{
     expect(emp.name).toBe(name)
 })
 
+test("Can instatiate Intern id from constructor", ()=>{
+    const id = "123"
+    const emp = new Intern(id);
+    expect(emp.id).toBe(emp.id)
+})
+
+test("Can instatiate Intern email from constructor", ()=>{
+    const email = "alanarmckeel@gmail.com"
+    const emp = new Intern(email);
+    expect(emp.email).toBe(emp.email)
+})
+
+// test for all methods in class
 // test for all methods in class
 test("Can instatiate Intern name from getName()", ()=>{
     const name = "Alana"
     const emp = new Intern(name);
     expect(emp.getName()).toBe(name)
+})
+
+test("Can instatiate Intern ID from getId()", ()=>{
+    const id = "123"
+    const emp = new Intern(id);
+    expect(emp.getId()).toBe(emp.id)
+})
+
+test("Can instatiate Intern email from getEmail()", ()=>{
+    const email = "alanarmckeel@mgmail.com"
+    const emp = new Intern(email);
+    expect(emp.getEmail()).toBe(emp.email)
+})
+
+test("Can instatiate Intern role from getRole()", ()=>{
+    const role = "role"
+    const emp = new Intern(role);
+    expect(emp.getRole()).toBe(emp.getRole())
+})
+
+test("Can instatiate Intern school from getSchool()", ()=>{
+    const school = "RMU"
+    const emp = new Intern(school);
+    expect(emp.school).toBe(emp.school)
 })
